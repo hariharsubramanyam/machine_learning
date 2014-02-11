186bbcbecb06ae9d9b94c88d86e0cd654ffabba2
Harihar Subramanyam <hsubrama@mit.edu>
2014-02-11 11:08:34 -0500

    Updated sample

--- perceptron.py
+++ perceptron.py
@@ -30,24 +30,28 @@ return: (theta, theta_zero) where theta is a d-dimensional numpy array and theta
 This represents the hypothesis:
 h(x;theta, theta_zero) = sign(dot(theta,x)+theta_zero)
 '''
-def perceptron(Sn, max_iterations=500, through_origin=False, verbose=False):
+def perceptron(Sn, max_iterations=500, through_origin=False, verbose=False, start_with=0, plot_after_misclassification=False, initial_theta=None, initial_theta_zero=None):
 	n = len(Sn)
 	if(n == 0):
 		raise Exception("Must have some training data!")
 	theta_zero = 0
 	theta = np.zeros(Sn[0][0].shape)
-	current_iteration = 0 			# keep track of the number of iterations
+	if initial_theta_zero is not None:
+		theta_zero = initial_theta_zero
+	if initial_theta is not None:
+		theta = initial_theta
+	current_iteration = start_with 			# keep track of the number of iterations
 	correct_classified_streak = 0	# how many examples have we correctly classified in a row? If we've classified all n of them, we've converged
 	while current_iteration < max_iterations and correct_classified_streak < n: # stop if we've exceeded max allowed iterations or if we've converged
-		correct_classified_streak += 1
 		index = current_iteration%n
 		(x,y) = Sn[index]
 		if verbose:
-			print "On iteration", current_iteration, "with streak", correct_classified_streak, "considering sample", index
+			print
+			print "On iteration", current_iteration, "with", correct_classified_streak, "correct classifications in a row. Now considering sample", (index+1)
 			print "Sample label:", y, "and features:", x
 		if y*(np.dot(theta,x) + theta_zero) <= 0:	# have we misclassified this training example?
 			if verbose:
-				print "misclassified sample"
+				print "Misclassified! Changing Theta"
 				print "old theta", theta, theta_zero
 			correct_classified_streak = 0
 			has_some_misclassifications = True
@@ -56,10 +60,18 @@ def perceptron(Sn, max_iterations=500, through_origin=False, verbose=False):
 				theta_zero = theta_zero + y 	# theta_zero update rule
 			if verbose:
 				print "new theta", theta, theta_zero
+			if plot_after_misclassification:
+				plot_2D_training(Sn, (theta, theta_zero))
+		else:
+			correct_classified_streak += 1
+			if verbose:
+				print "Correctly classified!"
 		current_iteration += 1
 	if verbose:
-		print "completed after", current_iteration, "iterations"
-		print "theta =", theta, "theta_zero=",theta_zero
+		print
+		print
+		print "completed after", current_iteration-1, "iterations"
+		print "theta =", theta, "theta_zero =",theta_zero
 		print
 		print
 	return (theta, theta_zero)
@@ -80,22 +92,38 @@ def plot_2D_training(Sn, theta_params=None):
 	neg_x = [x[0] for (x,y) in Sn if y==-1]
 	pos_y = [x[1] for (x,y) in Sn if y==1]
 	neg_y = [x[1] for (x,y) in Sn if y==-1]
-	plt.plot(pos_x, pos_y, "r+", ms=10)			# plot positive samples with a plus
+	plt.plot(pos_x, pos_y, "rs", ms=10)			# plot positive samples with a plus
 	plt.plot(neg_x, neg_y, "bo", ms=10)			# plot negative samples with an "x"
 	if theta_params is not None:			# if the theta params are avaiable, plot the decision boundary
 		(theta, theta_zero) = theta_params
 		X = [min(min(pos_x), min(neg_x))*1.5,max(max(pos_x), max(neg_x))*1.5]	# make sure the boundary covers the screen by finding the samples at the fringes of thes screen
-		Y = [(-1.0*theta_zero - theta[0]*X[0])/theta[1], (-1.0*theta_zero - theta[0]*X[1])/theta[1]] # find their corresponding y coordinates
+		if theta[1] == 0:
+			X = [theta_zero,theta_zero]
+			Y = [10, -10]
+		else:
+			Y = [(-1.0*theta_zero - theta[0]*X[0])/theta[1], (-1.0*theta_zero - theta[0]*X[1])/theta[1]] # find their corresponding y coordinates
 		plt.plot(X, Y, color="k", linestyle="-",linewidth=2)
 	plt.grid()
 	plt.show()
 
 
 if __name__ == "__main__":
+	A = np.array([[1,0,0],[0,1,0]])
+	
+	''' 
+	Ignore this:
 	Sn = [
-	(np.array([-1,1]), 1),
-	(np.array([0,-1]), -1),
-	(np.array([1.5,1]), 1)
+	(np.array([1,2,2]),-1),
+	(np.array([1,-2,2]),1)
 	]
 
-	plot_2D_training(Sn,perceptron(Sn, verbose=True, through_origin=True))
+	Sn = [(A.dot(x),y) for (x,y) in Sn]
+	'''
+
+	Sn = [
+	(np.array([1,2]),-1),
+	(np.array([-1,-2]),1)
+	]
+	theta_params = perceptron(Sn, verbose=True, through_origin=True, max_iterations=100, start_with=1, plot_after_misclassification=False)
+	plot_2D_training(Sn, theta_params)
+	
